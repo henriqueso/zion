2562cf8822813ab2ba6f97c7a508965334919831
henriqueso <henrique_d_oliveira@dell.com>
2016-06-05 21:12:41 -0300

    Change Position.java to implement Serializable

--- src/br/com/henriqueso/zion/board/Position.java
+++ src/br/com/henriqueso/zion/board/Position.java
@@ -1,7 +1,11 @@
 package br.com.henriqueso.zion.board;
 
-public class Position {
+import java.io.Serializable;
 
+public class Position implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
 	private int x;
 	private int y;
 	
