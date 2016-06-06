fae6d2ffd2ad81b539b6ea05aa1b8ec518292e1e
henriqueso <henrique_d_oliveira@dell.com>
2016-06-05 21:49:49 -0300

    Add Rook logic

--- src/br/com/henriqueso/zion/piece/Rook.java
+++ src/br/com/henriqueso/zion/piece/Rook.java
@@ -1,5 +1,6 @@
 package br.com.henriqueso.zion.piece;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.henriqueso.zion.board.ChessBoard;
@@ -7,12 +8,65 @@ import br.com.henriqueso.zion.board.Position;
 
 public class Rook extends ChessPiece {
 
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	public List<Position> threatens(ChessBoard chessBoard) {
-		// TODO: Implement how Rook attacks
-		return null;
+		List<Position> threatened = new ArrayList<>();
+		
+		right(chessBoard, threatened);
+		bottom(chessBoard, threatened);
+		left(chessBoard, threatened);
+		top(chessBoard, threatened);
+		
+		return threatened;
 	}
 
+	private void right(ChessBoard chessBoard, List<Position> threatened) {
+		int threatenedX = getPosition().getX();
+		int threatenedY = getPosition().getY() + 1;
+		
+		for (; threatenedY < chessBoard.getColumns(); threatenedY++) {
+			addPosition(chessBoard, threatened, threatenedX, threatenedY);	
+		}
+				
+	}
+
+	private void bottom(ChessBoard chessBoard, List<Position> threatened) {
+		int threatenedX = getPosition().getX() + 1;
+		int threatenedY = getPosition().getY();
+		
+		for (; threatenedX < chessBoard.getRows(); threatenedX++) {
+			addPosition(chessBoard, threatened, threatenedX, threatenedY);	
+		}
+	}
+
+	private void left(ChessBoard chessBoard, List<Position> threatened) {
+		int threatenedX = getPosition().getX();
+		int threatenedY = getPosition().getY() - 1;
+		
+		for (; threatenedY >= 0; threatenedY--) {
+			addPosition(chessBoard, threatened, threatenedX, threatenedY);	
+		}
+	}
+
+	private void top(ChessBoard chessBoard, List<Position> threatened) {
+		int threatenedX = getPosition().getX() - 1;
+		int threatenedY = getPosition().getY();
+	
+		for (; threatenedX >= 0; threatenedX--) {
+			addPosition(chessBoard, threatened, threatenedX, threatenedY);	
+		}
+	}
+
+	private void addPosition(final ChessBoard chessBoard, final List<Position> threatened, final int threatenedX, final int threatenedY) {
+		Position position = new Position(threatenedX, threatenedY);
+		
+		if (chessBoard.isValidPosition(position)){
+			threatened.add(position);
+		}
+	}
+	
 	@Override
 	public String getName() {
 		return "R";
---
+++ tst/br/com/henriqueso/zion/piece/RookTest.java
@@ -0,0 +1,266 @@
+package br.com.henriqueso.zion.piece;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import br.com.henriqueso.zion.board.ChessBoard;
+import br.com.henriqueso.zion.board.Position;
+
+public class RookTest {
+
+	@Test
+	public void testThreatensRookOn_0_0() {
+		/**
+		 * GIVEN a 3x3 ChessBoard
+		 *   AND Rook on position 0,0
+		 */
+		ChessBoard chessBoard = new ChessBoard(3,3);
+		ChessPiece piece = new Rook();
+		piece.setPosition(new Position(0, 0));
+		
+		/**
+		 *  WHEN getting the threatened positions
+		 */
+		List<Position> threatened = piece.threatens(chessBoard);
+		
+		/**
+		 *  THEN it should return positions 0,1 
+		 *   AND 0,2
+		 *   AND 1,0
+		 *   AND 2,0
+		 */
+		assertEquals(4, threatened.size());
+		assertEquals(new Position(0, 1), threatened.get(0));
+		assertEquals(new Position(0, 2), threatened.get(1));
+		assertEquals(new Position(1, 0), threatened.get(2));
+		assertEquals(new Position(2, 0), threatened.get(3));
+	}
+	
+	@Test
+	public void testThreatensRookOn_0_1() {
+		/**
+		 * GIVEN a 3x3 ChessBoard
+		 *   AND Rook on position 0,1
+		 */
+		ChessBoard chessBoard = new ChessBoard(3,3);
+		ChessPiece piece = new Rook();
+		piece.setPosition(new Position(0, 1));
+		
+		/**
+		 *  WHEN getting the threatened positions
+		 */
+		List<Position> threatened = piece.threatens(chessBoard);
+		
+		/**
+		 *  THEN it should return positions 0,2 
+		 *   AND 1,1
+		 *   AND 2,1
+		 *   AND 0,0
+		 */
+		assertEquals(4, threatened.size());
+		assertEquals(new Position(0, 2), threatened.get(0));
+		assertEquals(new Position(1, 1), threatened.get(1));
+		assertEquals(new Position(2, 1), threatened.get(2));
+		assertEquals(new Position(0, 0), threatened.get(3));
+	}
+	
+	@Test
+	public void testThreatensRookOn_0_2() {
+		/**
+		 * GIVEN a 3x3 ChessBoard
+		 *   AND Rook on position 0,2
+		 */
+		ChessBoard chessBoard = new ChessBoard(3,3);
+		ChessPiece piece = new Rook();
+		piece.setPosition(new Position(0, 2));
+		
+		/**
+		 *  WHEN getting the threatened positions
+		 */
+		List<Position> threatened = piece.threatens(chessBoard);
+		
+		/**
+		 *  THEN it should return positions 1,2 
+		 *   AND 2,2
+		 *   AND 0,1
+		 *   AND 0,0
+		 */
+		assertEquals(4, threatened.size());
+		assertEquals(new Position(1, 2), threatened.get(0));
+		assertEquals(new Position(2, 2), threatened.get(1));
+		assertEquals(new Position(0, 1), threatened.get(2));
+		assertEquals(new Position(0, 0), threatened.get(3));
+	}
+	
+	@Test
+	public void testThreatensRookOn_1_0() {
+		/**
+		 * GIVEN a 3x3 ChessBoard
+		 *   AND Rook on position 1,0
+		 */
+		ChessBoard chessBoard = new ChessBoard(3,3);
+		ChessPiece piece = new Rook();
+		piece.setPosition(new Position(1, 0));
+		
+		/**
+		 *  WHEN getting the threatened positions
+		 */
+		List<Position> threatened = piece.threatens(chessBoard);
+		
+		/**
+		 *  THEN it should return positions 1,1 
+		 *   AND 1,2
+		 *   AND 2,0
+		 *   AND 0,0
+		 */
+		assertEquals(4, threatened.size());
+		assertEquals(new Position(1, 1), threatened.get(0));
+		assertEquals(new Position(1, 2), threatened.get(1));
+		assertEquals(new Position(2, 0), threatened.get(2));
+		assertEquals(new Position(0, 0), threatened.get(3));
+	}
+	
+	@Test
+	public void testThreatensRookOn_1_1() {
+		/**
+		 * GIVEN a 3x3 ChessBoard
+		 *   AND Rook on position 1,1
+		 */
+		ChessBoard chessBoard = new ChessBoard(3,3);
+		ChessPiece piece = new Rook();
+		piece.setPosition(new Position(1, 1));
+		
+		/**
+		 *  WHEN getting the threatened positions
+		 */
+		List<Position> threatened = piece.threatens(chessBoard);
+		
+		/**
+		 *  THEN it should return positions 1,2 
+		 *   AND 2,1
+		 *   AND 1,0
+		 *   AND 0,1
+		 */
+		assertEquals(4, threatened.size());
+		assertEquals(new Position(1, 2), threatened.get(0));
+		assertEquals(new Position(2, 1), threatened.get(1));
+		assertEquals(new Position(1, 0), threatened.get(2));
+		assertEquals(new Position(0, 1), threatened.get(3));
+	}
+	
+	@Test
+	public void testThreatensRookOn_1_2() {
+		/**
+		 * GIVEN a 3x3 ChessBoard
+		 *   AND Rook on position 1,2
+		 */
+		ChessBoard chessBoard = new ChessBoard(3,3);
+		ChessPiece piece = new Rook();
+		piece.setPosition(new Position(1, 2));
+		
+		/**
+		 *  WHEN getting the threatened positions
+		 */
+		List<Position> threatened = piece.threatens(chessBoard);
+		
+		/**
+		 *  THEN it should return positions 2,2 
+		 *   AND 1,1
+		 *   AND 1,0
+		 *   AND 0,2
+		 */
+		assertEquals(4, threatened.size());
+		assertEquals(new Position(2, 2), threatened.get(0));
+		assertEquals(new Position(1, 1), threatened.get(1));
+		assertEquals(new Position(1, 0), threatened.get(2));
+		assertEquals(new Position(0, 2), threatened.get(3));
+	}
+	
+	@Test
+	public void testThreatensRookOn_2_0() {
+		/**
+		 * GIVEN a 3x3 ChessBoard
+		 *   AND Rook on position 2,0
+		 */
+		ChessBoard chessBoard = new ChessBoard(3,3);
+		ChessPiece piece = new Rook();
+		piece.setPosition(new Position(2, 0));
+		
+		/**
+		 *  WHEN getting the threatened positions
+		 */
+		List<Position> threatened = piece.threatens(chessBoard);
+		
+		/**
+		 *  THEN it should return positions 2,1 
+		 *   AND 2,2
+		 *   AND 1,0
+		 *   AND 0,0
+		 */
+		assertEquals(4, threatened.size());
+		assertEquals(new Position(2, 1), threatened.get(0));
+		assertEquals(new Position(2, 2), threatened.get(1));
+		assertEquals(new Position(1, 0), threatened.get(2));
+		assertEquals(new Position(0, 0), threatened.get(3));
+	}
+	
+	@Test
+	public void testThreatensRookOn_2_1() {
+		/**
+		 * GIVEN a 3x3 ChessBoard
+		 *   AND Rook on position 2,1
+		 */
+		ChessBoard chessBoard = new ChessBoard(3,3);
+		ChessPiece piece = new Rook();
+		piece.setPosition(new Position(2, 1));
+		
+		/**
+		 *  WHEN getting the threatened positions
+		 */
+		List<Position> threatened = piece.threatens(chessBoard);
+		
+		/**
+		 *  THEN it should return positions 2,2 
+		 *   AND 2,0
+		 *   AND 1,1
+		 *   AND 0,1
+		 */
+		assertEquals(4, threatened.size());
+		assertEquals(new Position(2, 2), threatened.get(0));
+		assertEquals(new Position(2, 0), threatened.get(1));
+		assertEquals(new Position(1, 1), threatened.get(2));
+		assertEquals(new Position(0, 1), threatened.get(3));
+	}
+	
+	@Test
+	public void testThreatensRookOn_2_2() {
+		/**
+		 * GIVEN a 3x3 ChessBoard
+		 *   AND Rook on position 2,2
+		 */
+		ChessBoard chessBoard = new ChessBoard(3,3);
+		ChessPiece piece = new Rook();
+		piece.setPosition(new Position(2, 2));
+		
+		/**
+		 *  WHEN getting the threatened positions
+		 */
+		List<Position> threatened = piece.threatens(chessBoard);
+		
+		/**
+		 *  THEN it should return positions 2,1 
+		 *   AND 2,0
+		 *   AND 1,2
+		 *   AND 0,2
+		 */
+		assertEquals(4, threatened.size());
+		assertEquals(new Position(2, 1), threatened.get(0));
+		assertEquals(new Position(2, 0), threatened.get(1));
+		assertEquals(new Position(1, 2), threatened.get(2));
+		assertEquals(new Position(0, 2), threatened.get(3));
+	}
+
+}
